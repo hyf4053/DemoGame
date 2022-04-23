1e891452be50d2c900cebe1d2b581e9689d2cc1c
Lanmaomi <hyf4053@hotmail.com>
2022-04-23 21:43:07 +0800

    Initial commit

---
+++ Source/DemoGame/DemoGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DemoGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DemoGame, "DemoGame" );
---
+++ Source/DemoGame/DemoGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DemoGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMOGAME_API ADemoGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
